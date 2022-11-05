744fb6e92f6dddc537f30403dead97cf1bf672b4
Isaac McCracken <isaac.mccracken@gmail.com>
2022-11-05 15:47:14 -0600

    axial.py

--- axial.py
+++ axial.py
@@ -9,6 +9,6 @@ def screen_to_axial(vector2, size):
     return (q, r)
 
 def axial_to_screen(axial, size):
-    x = (1.5*axial[0])*size
-    y = (ROOT3*axial[0]/2.0 + ROOT3*axial[1])*size
+    x = (1.5*axial[0])/size
+    y = (ROOT3*axial[0]/2.0 + ROOT3*axial[1])/size
     return (x, y)
