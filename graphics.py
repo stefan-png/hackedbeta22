1d88f3013721030a0c1a9d77bbfdf287e36d2af1
Stefan <stefan2play@gmail.com>
2022-11-05 19:14:19 -0600

    added pieces drawing

---
+++ graphics.py
@@ -0,0 +1,96 @@
+import pygame, math, numpy
+
+light_colour = (216, 189, 138)
+mid_colour = (170, 80, 66)
+dark_colour = (79, 49, 48)
+
+tile_colours = [light_colour, mid_colour, dark_colour]
+
+SQRT32 = math.sqrt(3)/2
+SQRT3 = math.sqrt(3)
+hex_offset = [  
+                (-0.5, SQRT32),
+                (0.5, SQRT32),
+                (1, 0),
+                (0.5, -SQRT32),
+                (-0.5, -SQRT32),
+                (-1, 0),
+            ]
+
+def draw_hex(surface, pos, scale, colour, width=0):
+    
+    transformed_offset = []
+    for coord in hex_offset:
+        # coord * scale + pos
+        transformed_offset.append(numpy.add(numpy.array(coord)* scale, pos))
+
+    pygame.draw.polygon(surface, colour, transformed_offset, width)
+
+def draw_board(surface, pos, scale):
+    
+    yoffset = scale*SQRT3
+    half_yoffset = yoffset*0.5
+    # draw centreline
+    # for y in range(-5, 6):
+    #     draw_hex(numpy.add(origin, (0, yoffset*y)), scale,  tile_colours[(y+5)%3])
+
+    # draw sides
+    for x in range(6):
+        for y in range(-5, 6-x):
+            draw_hex(surface, numpy.add(pos, (x*1.5*scale, y*yoffset+half_yoffset*x)), scale,  tile_colours[(x+2*y+5)%3])
+            draw_hex(surface, numpy.add(pos, (-x*1.5*scale, y*yoffset+half_yoffset*x)), scale,  tile_colours[(x+2*y+5)%3])
+
+def load_spritesheet(filename):
+        """Load the sheet."""
+        try:
+            return pygame.image.load(filename).convert_alpha()
+        except pygame.error as e:
+            print(f"Unable to load spritesheet image: {filename}")
+            raise SystemExit(e)
+
+# spritesheet is the sheet to sample from
+# rectangle is the image in screen coordinates
+# scale is the final scale to draw the image in (width, height)
+def image_at(spritesheet, rectangle):
+        """Load a specific image from a specific rectangle."""
+        # Loads image from x, y, x+offset, y+offset.
+        rect = pygame.Rect(rectangle)
+        image = pygame.Surface(rect.size).convert_alpha()
+        image.blit(spritesheet, (0, 0), rect)
+        return image
+
+# surface to draw on
+# image to draw
+# position to draw at
+# scale is (width, height)
+def draw_image(surface, image, pos, size):
+        rect = image.get_rect()
+        rect.topleft = pos
+        image = pygame.transform.scale(image, size)
+        surface.blit(image, rect)
+
+def draw_piece(surface, spritesheet, pos, scale, pieceID, colour):
+    kingrect = pygame.Rect(0, 0, 300, 300)
+    queenrect = pygame.Rect(300, 0, 300, 300)
+    bishrect = pygame.Rect(600, 0, 300, 300)
+    horserect = pygame.Rect(900, 0, 300, 300)
+    rookrect = pygame.Rect(1200, 0, 300, 300)
+    pawnrect = pygame.Rect(1500, 0, 300, 300)
+    
+    rect = pawnrect
+
+    if pieceID == "king":
+        rect = kingrect
+    elif pieceID == "queen":
+        rect = queenrect
+    elif pieceID == "bishop":
+        rect = bishrect
+    elif pieceID == "knight":
+        rect = horserect
+    elif pieceID == "rook":
+        rect = rookrect
+
+    if colour == "black":
+        rect.y += 300
+
+    draw_image(surface, image_at(spritesheet, rect), numpy.subtract(pos, (0.75*scale, 0.80*scale)), (1.5*scale, 1.5*scale))
--- pygametest.py
+++ pygametest.py
@@ -1,74 +1,39 @@
 import sys, pygame, math
 import numpy
 
-import axial
-
-SQRT32 = math.sqrt(3)/2
-SQRT3 = math.sqrt(3)
-hex_offset = [  
-                (-0.5, SQRT32),
-                (0.5, SQRT32),
-                (1, 0),
-                (0.5, -SQRT32),
-                (-0.5, -SQRT32),
-                (-1, 0),
-            ]
-
-light_colour = (216, 189, 138)
-mid_colour = (170, 80, 66)
-dark_colour = (79, 49, 48)
-
-tile_colours = [light_colour, mid_colour, dark_colour]
-
+import axial, graphics as g
 
 screen_width = 800
 screen_height = 800
 origin = (screen_width/2, screen_height/2)
 
-def draw_hex(pos, scale, colour, width=0):
-    
-    transformed_offset = []
-    for coord in hex_offset:
-        # coord * scale + pos
-        transformed_offset.append(numpy.add(numpy.array(coord)* scale, pos))
-
-    pygame.draw.polygon(surface, colour, transformed_offset, width)
-
-def draw_board(scale):
-    
-    yoffset = scale*SQRT3
-    half_yoffset = yoffset*0.5
-    # draw centreline
-    # for y in range(-5, 6):
-    #     draw_hex(numpy.add(origin, (0, yoffset*y)), scale,  tile_colours[(y+5)%3])
-
-    # draw sides
-    for x in range(6):
-        for y in range(-5, 6-x):
-            draw_hex(numpy.add(origin, (x*1.5*scale, y*yoffset+half_yoffset*x)), scale,  tile_colours[(x+2*y+5)%3])
-            draw_hex(numpy.add(origin, (-x*1.5*scale, y*yoffset+half_yoffset*x)), scale,  tile_colours[(x+2*y+5)%3])
-
 pygame.init()
-
 surface = pygame.display.set_mode((screen_width, screen_height))
+
+spritesheet = g.load_spritesheet("chesspieces.png")
 font = pygame.font.SysFont(None, 24)
 
 # game loop
 while True:
 
     # state
-    scale = 30
+    scale = 40
 
     # draw background
     surface.fill((255, 255, 255, 255))
-    draw_board(scale)
+    g.draw_board(surface, origin, scale)
 
     mousepos = pygame.mouse.get_pos()
 
     axialpos = axial.axial_round(axial.screen_to_axial(numpy.subtract(mousepos, origin), scale))
     pos = numpy.add(axial.axial_to_screen(axialpos, scale), origin)
-    draw_hex(pos, scale, (230, 230, 240), 4)
+    g.draw_hex(surface, pos, scale, (230, 230, 240), 4)
     pygame.draw.circle(surface, (255, 255, 255), pos, scale/3)
+    g.draw_piece(surface, spritesheet, pos, scale, "knight", "white")
+    g.draw_piece(surface, spritesheet, numpy.add(axial.axial_to_screen((1, 0), scale), origin), scale, "queen", "white")
+    g.draw_piece(surface, spritesheet, numpy.add(axial.axial_to_screen((2, 0), scale), origin), scale, "rook", "white")
+    g.draw_piece(surface, spritesheet, numpy.add(axial.axial_to_screen((1, 1), scale), origin), scale, "bishop", "black")
+    g.draw_piece(surface, spritesheet, numpy.add(axial.axial_to_screen((2, 1), scale), origin), scale, "king", "black")
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
