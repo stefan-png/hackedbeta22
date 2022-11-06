e4044e1c4f191c03397e1a5466ec06bb5e09c55d
Peter O'Keeffe <peter.okeeffe11@hotmail.com>
2022-11-05 21:21:39 -0600

    works

--- Evaluation.py
+++ Evaluation.py
@@ -1,11 +1,17 @@
+from Board import *
+
+grid = set_Up_Board(mode= 'glinski')
+
 def check_If_Valid(pos,pos_start):  # checks if the position is valid
-    object_start = grid(pos_start)
-    object= grid(pos)
+    object_start = grid[pos_start]
     if (0 > pos[0]) or (pos[0] > 10) or (0 > pos[1]) or (pos[1] > 10) or (pos[0] + pos[1] > 15) or (
             pos[0] + pos[1] < 5):
         return False
-    elif object_pos != 0:
-        if object.colour == object_start.colour:
+
+    object_pos = grid[pos]
+
+    if type(object_pos) == object:
+        if object_pos.colour == object_start.colour:
             return false
     elif in_Check() == True:
         return false
