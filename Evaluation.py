8c809624a72e67fac42a4166814f0e7c6199a0bb
Peter O'Keeffe <peter.okeeffe11@hotmail.com>
2022-11-05 22:12:19 -0600

    haha

--- Evaluation.py
+++ Evaluation.py
@@ -1,18 +1,18 @@
+import numpy as np
 from Board import *
 
 grid = set_Up_Board(mode= 'glinski')
 
-def check_If_Valid(pos,pos_start):  # checks if the position is valid
+def check_If_Valid(np_pos,np_pos_start):# checks if the position is valid
+    pos_start = (np_pos_start[0],np_pos_start[1])
+    pos = (np_pos[0],np_pos[1])
     object_start = grid[pos_start]
-    if (0 > pos[0]) or (pos[0] > 10) or (0 > pos[1]) or (pos[1] > 10) or (pos[0] + pos[1] > 15) or (
-            pos[0] + pos[1] < 5):
+    if (0 > pos[0]) or (pos[0] > 10) or (0 > pos[1]) or (pos[1] > 10) or (pos[0] + pos[1] > 15) or (pos[0] + pos[1] < 5):
         return False
-
     object_pos = grid[pos]
-
-    if type(object_pos) == object:
+    if object_pos != 0 and pos != pos_start:
         if object_pos.colour == object_start.colour:
-            return false
+            return False
     elif in_Check() == True:
         return false
     else:
