7fc9fdab3eb98ac7f508b9f390a77553556c0ccb
Stefan <stefan2play@gmail.com>
2022-11-06 10:59:38 -0700

    game loop

--- pygametest.py
+++ pygametest.py
@@ -35,6 +35,9 @@ if __name__=="__main__":
 
     selected_tile = NONE
 
+    taken_white_pieces = numpy.zeros(7)
+    taken_black_pieces = numpy.zeros(7)
+
     # game loop
     while True:
 
@@ -62,22 +65,30 @@ if __name__=="__main__":
                     selected_tile = 0
                     continue
 
-                # if selected_tile != 0:
-                #     print(Eval.possible_moves(grid, flipxy(selected_tile)))
-                #     print(check_if_possible_move(grid, flipxy(selected_tile), flipxy(picked_pos)))
-                #     print(picked_pos)
-               
                 if selected_tile == 0 or grid[flipxy(selected_tile)] == 0:
                     # we havent selected a tile yet, select one
                     selected_tile = picked_pos
                 elif picked_pos == selected_tile:
                     # we picked the same as is selected
                     selected_tile = 0
-                elif check_if_possible_move(grid, flipxy(selected_tile), flipxy(picked_pos)):
+                elif check_if_possible_move(grid, flipxy(selected_tile), flipxy(picked_pos)) and grid[flipxy(selected_tile)].colour == turn:
                     
                     # we picked a different tile than is selected
                     # if its a valid move, make the move
-                    # TODO check if its my turn
+
+                    # TODO if we are going to eat an enemy piece, move it to the side.
+                    piece_idx = NONE
+                    if grid[flipxy(picked_pos)] not in [0, 1]:
+                        piece_idx = grid[flipxy(picked_pos)].type
+                    if turn == WHITE:
+                        turn = BLACK
+                        taken_black_pieces[piece_idx] += 1
+                        print("taken black pieces", taken_black_pieces)
+                    else:
+                        turn = WHITE
+                        taken_white_pieces[piece_idx] += 1
+                        print("taken white pieces", taken_white_pieces)
+
                     grid[picked_pos[1], picked_pos[0]] = grid[selected_tile[1], selected_tile[0]]
                     grid[selected_tile[1], selected_tile[0]] = 0
                     selected_tile = 0
@@ -85,42 +96,6 @@ if __name__=="__main__":
                     # otherwise, its an invalid move, select the picked tile
                     selected_tile = picked_pos
 
-                # if we picked a tile, and we cant move there, select it.
-                """
-                if not Eval.check_on_board(flipxy(picked_pos)):
-                    selected_tile = 0
-                    continue
-                # can assume user clicked on valid tile
- 
-                if selected_tile != 0:
-                    # if selection is a valid tile
-                        
-                    if selected_tile == picked_pos: 
-                        # clicked on selected tile, toggle the selected tile
-                        selected_tile = 0
-                    elif Eval.check_If_Valid(grid, flipxy(picked_pos), flipxy(selected_tile)):
-                        # try to snap to picked pos
-                        # TODO check if move is enemy, add point, etc etc
-                        grid[picked_pos[1], picked_pos[0]] = grid[selected_tile[1], selected_tile[0]]
-                        grid[selected_tile[1], selected_tile[0]] = 0
-                        selected_tile = 0
-                    else:
-                        # attempted move is invalid.
-                        selected_tile = 0
-                    
-                else:
-                    # didn't have any point selected, select this square
-                    selected_tile = picked_pos
-                    if grid[picked_pos[1], picked_pos[0]] not in [0, 1]:
-                        # check if picked pos has a piece on it. if so, hold it
-                        held_piece = grid[picked_pos[1], picked_pos[0]]
-                        held_peice_last_pos = picked_pos
-                        grid[picked_pos[1], picked_pos[0]] = 0
-                    else:
-                        # if picked pos has no piece, reset hold variables
-                        held_piece = 0
-                        held_peice_last_pos = 0
-                        """
             if event.type == pygame.MOUSEBUTTONUP or event.type == pygame.WINDOWLEAVE:            
                 #check if on board
                 # if user released mouse on selected tile, unselect it and do nothing else
@@ -128,24 +103,8 @@ if __name__=="__main__":
                     continue
 
                 # if user released mouse 
-                """
-
-                if Eval.check_on_board(flipxy(picked_pos)) and Eval.check_If_Valid(grid, flipxy(picked_pos), flipxy(selected_tile)):
-
-                    # dragged to valid tile, move the piece
-                    grid[picked_pos[1], picked_pos[0]] = held_piece
-                    
-
-                else:
-                    # dragged to invalid square, replace the piece
-                    grid[held_peice_last_pos[1], held_peice_last_pos[0]] = held_piece
-                
-                if held_peice_last_pos != picked_pos:
-                    selected_tile = 0
-
-                held_piece = 0
-                held_peice_last_pos = 0
-                """
+        
+        # *** end event handling ***
 
         # highlight whichever tile is selected
         if selected_tile != 0:
@@ -157,7 +116,7 @@ if __name__=="__main__":
             g.draw_piece(surface, spritesheet, mousepos, scale, held_piece.type, held_piece.colour)
 
         # draw all possible moves for selected piece
-        if selected_tile != 0 and grid[selected_tile[1], selected_tile[0]] not in [0, 1]:
+        if selected_tile != 0 and grid[selected_tile[1], selected_tile[0]] not in [0, 1] and grid[flipxy(selected_tile)].colour == turn:
             # draw dot on each possible move
             for move in Eval.possible_moves(grid, (selected_tile[1], selected_tile[0])):
                 pygame.draw.circle(surface, g.get_hex_colour(move[1], move[0]+2), numpy.add(axial.axial_to_screen((move[1], move[0]), scale), peter_offset), scale/3)
