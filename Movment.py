7e636a30f07ae1e19c4441d7f5e655dee8eb80c9
Peter O'Keeffe <peter.okeeffe11@hotmail.com>
2022-11-05 21:09:11 -0600

    haha

--- Movment.py
+++ Movment.py
@@ -1,95 +1,90 @@
 import numpy as np
+import Evaluation
 
-def king(pos):
+def king(pos): # returns list of all possible king moves
     moves = []
     m = [(1,1),(1,-1),(1,0),(-1,1),(-1,-1),(-1,0),(0,1),(0,-1),(2,-1),(-2,1),(1,-2),(-1,2)]
     for i in m:
-        if check_If_Valid(np.add(pos,i)) == True:
+        if check_If_Valid(np.add(pos,i), grid(pos)) == True:
             moves.append(np.add(pos,i))
     disp(pos, moves)
     return moves
 
-def rook(pos):
+def rook(pos): # returns list of all possible rook moves
         moves = [pos]
 
-        while check_If_Valid(moves[-1]) == True:
+        while check_If_Valid(moves[-1], pos) == True:
             moves.append(np.add(moves[-1], (0,1)))
         moves[-1] = pos
 
-        while check_If_Valid(moves[-1]) == True:
+        while check_If_Valid(moves[-1], pos) == True:
             moves.append(np.subtract(moves[-1],(0,1)))
         moves[-1] = pos
 
-        while check_If_Valid(moves[-1]) == True:
+        while check_If_Valid(moves[-1], pos) == True:
             moves.append(np.add(moves[-1],(1,0)))
         moves[-1] = pos
 
-        while check_If_Valid(moves[-1]) == True:
+        while check_If_Valid(moves[-1], pos) == True:
             moves.append(np.subtract(moves[-1],(1,0)))
         moves[-1] = pos
 
-        while check_If_Valid(moves[-1]) == True:
+        while check_If_Valid(moves[-1], pos) == True:
             moves.append(np.add(moves[-1], (1, -1)))
         moves[-1] = pos
 
-        while check_If_Valid(moves[-1]) == True:
+        while check_If_Valid(moves[-1], pos) == True:
             moves.append(np.add(moves[-1], (-1, 1)))
         moves[-1] = pos
 
         disp(pos, moves)
         return moves
 
-def bishop(pos):
+def bishop(pos): # returns list of all possible bishop moves
     moves = [pos]
 
-    while check_If_Valid(moves[-1]) == True:
+    while check_If_Valid(moves[-1], pos) == True:
         moves.append(np.add(moves[-1], (1,1)))
     moves[-1] = pos
 
-    while check_If_Valid(moves[-1]) == True:
+    while check_If_Valid(moves[-1], pos) == True:
         moves.append(np.subtract(moves[-1], (1,1)))
     moves[-1] = pos
 
-    while check_If_Valid(moves[-1]) == True:
+    while check_If_Valid(moves[-1], pos) == True:
         moves.append(np.add(moves[-1], (1,-2)))
     moves[-1] = pos
 
-    while check_If_Valid(moves[-1]) == True:
+    while check_If_Valid(moves[-1], pos) == True:
         moves.append(np.subtract(moves[-1], (1,-2)))
     moves[-1] = pos
 
-    while check_If_Valid(moves[-1]) == True:
+    while check_If_Valid(moves[-1], pos) == True:
         moves.append(np.add(moves[-1], (2,-1)))
     moves[-1] = pos
 
-    while check_If_Valid(moves[-1]) == True:
+    while check_If_Valid(moves[-1], pos) == True:
         moves.append(np.subtract(moves[-1], (2,-1)))
     moves[-1] = pos
 
     disp(pos, moves)
     return moves
 
-def knight(pos):
+def knight(pos): # returns list of all possible knight moves
     moves = [pos]
     m = [(1,-3),(-1,-2),(-2,-1),(-3,1),(-3,2),(-2,3),(-1,3),(1,2),(2,1),(3,-1),(3,-2),(2,-3)]
     for i in m:
-        if check_If_Valid(np.add(pos,i)) == True:
+        if check_If_Valid(np.add(pos,i), pos) == True:
             moves.append(np.add(pos,i))
     disp(pos, moves)
     return moves
 
-def queen(pos):
+def queen(pos): # retunrs list of all possible queen moves
     moves = rook(pos) + bishop(pos)
     disp(pos,moves)
     return moves
 
-def check_If_Valid(pos):
-    if (0 > pos[0]) or (pos[0] > 10) or (0>pos[1]) or (pos[1] >10) or (pos[0] + pos[1] > 15) or (pos[0] + pos[1] < 5):
-        return False
-    else:
-        return True
-
-def disp(pos, moves):
+def disp(pos, moves): # diplays the possible moves on a terminal grid
     grid = np.ones(shape= (11,11))
     for q in range(0,11):
         for r in range(0,11):
@@ -101,6 +96,4 @@ def disp(pos, moves):
         grid[(pos[0],pos[1])] = 9
 
     print(grid)
-
-queen([5,5])
 
