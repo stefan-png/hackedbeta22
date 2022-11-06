3e5876caca9d49e03b89640722940336638abe18
Stefan <stefan2play@gmail.com>
2022-11-06 10:12:06 -0700

    can only make valid moves

--- pygametest.py
+++ pygametest.py
@@ -1,9 +1,17 @@
 import sys, pygame, math
 import numpy
 import axial, graphics as g
-import Evaluation
+import Evaluation as Eval
 from Board import *
 
+def flipxy(tuple):
+    return (tuple[1], tuple[0])
+
+def check_if_possible_move(grid, startpos, endpos):
+    for move in Eval.possible_moves(grid, startpos):
+        if tuple(move) == endpos:
+            return True
+    return False
 
 screen_width = 800
 screen_height = 800
@@ -12,7 +20,7 @@ origin = (screen_width/2, screen_height/2)
 # setup board
 grid = set_Up_Board()
 turn = WHITE
-print(Evaluation.check_If_Valid(grid, (4,4), (10,4)))
+print(Eval.check_If_Valid(grid, (4,4), (10,4)))
 if __name__=="__main__":
 
     pygame.init()
@@ -49,51 +57,80 @@ if __name__=="__main__":
             if event.type == pygame.QUIT: 
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN: 
-
-                if not Evaluation.check_on_board(picked_pos):
+                # make sure the picked_pos is on the board
+                if not Eval.check_on_board(flipxy(picked_pos)):
                     selected_tile = 0
                     continue
 
-                if grid[picked_pos[1], picked_pos[0]] == 0:
-                    # picked a blank square
-                    # TODO change to check for valid move
-                    if selected_tile != 0:
+                # if selected_tile != 0:
+                #     print(Eval.possible_moves(grid, flipxy(selected_tile)))
+                #     print(check_if_possible_move(grid, flipxy(selected_tile), flipxy(picked_pos)))
+                #     print(picked_pos)
+               
+                if selected_tile == 0 or grid[flipxy(selected_tile)] == 0:
+                    # we havent selected a tile yet, select one
+                    selected_tile = picked_pos
+                elif picked_pos == selected_tile:
+                    # we picked the same as is selected
+                    selected_tile = 0
+                elif check_if_possible_move(grid, flipxy(selected_tile), flipxy(picked_pos)):
+                    
+                    # we picked a different tile than is selected
+                    # if its a valid move, make the move
+                    # TODO check if its my turn
+                    grid[picked_pos[1], picked_pos[0]] = grid[selected_tile[1], selected_tile[0]]
+                    grid[selected_tile[1], selected_tile[0]] = 0
+                    selected_tile = 0
+                else:
+                    # otherwise, its an invalid move, select the picked tile
+                    selected_tile = picked_pos
+
+                # if we picked a tile, and we cant move there, select it.
+                """
+                if not Eval.check_on_board(flipxy(picked_pos)):
+                    selected_tile = 0
+                    continue
+                # can assume user clicked on valid tile
+ 
+                if selected_tile != 0:
+                    # if selection is a valid tile
                         
-
-                        if selected_tile == picked_pos:
-                            selected_tile = 0
-                        else:
-
-                            # try to snap to picked pos
-                            grid[picked_pos[1], picked_pos[0]] = grid[selected_tile[1], selected_tile[0]]
-                            grid[selected_tile[1], selected_tile[0]] = 0
-                            selected_tile = 0
-                        
-                    else:
-                        # didn't have any point selected, select this square
-                        selected_tile = picked_pos
-
-                    # make sure to reset any hold variables
-                    held_piece = 0
-                    held_peice_last_pos = 0
-
-                elif grid[picked_pos[1], picked_pos[0]] not in [0, 1]:
-                    # picked a piece
-                    if selected_tile == picked_pos:
+                    if selected_tile == picked_pos: 
+                        # clicked on selected tile, toggle the selected tile
+                        selected_tile = 0
+                    elif Eval.check_If_Valid(grid, flipxy(picked_pos), flipxy(selected_tile)):
+                        # try to snap to picked pos
+                        # TODO check if move is enemy, add point, etc etc
+                        grid[picked_pos[1], picked_pos[0]] = grid[selected_tile[1], selected_tile[0]]
+                        grid[selected_tile[1], selected_tile[0]] = 0
                         selected_tile = 0
                     else:
-                        selected_tile = picked_pos
+                        # attempted move is invalid.
+                        selected_tile = 0
+                    
+                else:
+                    # didn't have any point selected, select this square
+                    selected_tile = picked_pos
+                    if grid[picked_pos[1], picked_pos[0]] not in [0, 1]:
+                        # check if picked pos has a piece on it. if so, hold it
                         held_piece = grid[picked_pos[1], picked_pos[0]]
                         held_peice_last_pos = picked_pos
                         grid[picked_pos[1], picked_pos[0]] = 0
-                        
+                    else:
+                        # if picked pos has no piece, reset hold variables
+                        held_piece = 0
+                        held_peice_last_pos = 0
+                        """
             if event.type == pygame.MOUSEBUTTONUP or event.type == pygame.WINDOWLEAVE:            
                 #check if on board
-                
+                # if user released mouse on selected tile, unselect it and do nothing else
                 if held_peice_last_pos == 0:
                     continue
 
-                if Evaluation.check_on_board(picked_pos) and grid[picked_pos[1], picked_pos[0]] == 0:
+                # if user released mouse 
+                """
+
+                if Eval.check_on_board(flipxy(picked_pos)) and Eval.check_If_Valid(grid, flipxy(picked_pos), flipxy(selected_tile)):
 
                     # dragged to valid tile, move the piece
                     grid[picked_pos[1], picked_pos[0]] = held_piece
@@ -108,7 +145,7 @@ if __name__=="__main__":
 
                 held_piece = 0
                 held_peice_last_pos = 0
-
+                """
 
         # highlight whichever tile is selected
         if selected_tile != 0:
@@ -122,7 +159,7 @@ if __name__=="__main__":
         # draw all possible moves for selected piece
         if selected_tile != 0 and grid[selected_tile[1], selected_tile[0]] not in [0, 1]:
             # draw dot on each possible move
-            for move in Evaluation.possible_moves(grid, (selected_tile[1], selected_tile[0])):
+            for move in Eval.possible_moves(grid, (selected_tile[1], selected_tile[0])):
                 pygame.draw.circle(surface, g.get_hex_colour(move[1], move[0]+2), numpy.add(axial.axial_to_screen((move[1], move[0]), scale), peter_offset), scale/3)
 
         # draw debug text
